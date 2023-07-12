4239f392609da841fe67d9b484688fc5edad7e1c
Chirag <chirag.dev18@gmail.com>
2023-07-12 22:41:16 +0530

    Added homepage ProductCards

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Image from "next/image"
 import Link from "next/link"
 
@@ -12,14 +13,30 @@ export default function Navbar() {
             <li className="px-4 py-2"><Link href="">Delivery</Link></li>
         </ul>
         <div className="flex bg-gray-200 rounded-3xl px-4 py-1">
-          <input type="search" className=" w-64 bg-transparent focus:border-transparent" placeholder="Search Product" />
-          <Image src="/search.svg" alt="btn" width="24" height="16" />
-
+          <input type="search" className=" w-64 bg-transparent focus:outline-none" placeholder="Search Product" />
+          <Image src="/search.svg" alt="btn" width="24" height="16" 
+          className="hover:cursor-pointer"
+          onClick={()=>{
+            alert("Clicked")
+          }}
+          />
         </div>
 
-        <div className="mt-2">
-            <Link href="" className="px-4 py-2 rounded border mr-4">Account</Link>
-            <Link href="" className="px-4 py-2 rounded border">Cart</Link>
+        <div className="mt-2 flex">  
+            <Link href="" className="mr-8 flex gap-2">
+              <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 18">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 8a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm-2 3h4a4 4 0 0 1 4 4v2H1v-2a4 4 0 0 1 4-4Z"/>
+              </svg>
+              Account
+            </Link>
+          
+            <Link href="" className="flex gap-2">
+              <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 15a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm0 0h8m-8 0-1-4m9 4a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-9-4h10l2-7H3m2 7L3 4m0 0-.792-3H1"/>
+              </svg>
+              Cart
+            </Link>
+          
         </div>
       </nav>
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,47 @@
-import Image from 'next/image'
+// import Image from 'next/image'
 
 import Navbar from './components/navbar'
 import Link from 'next/link'
 
+const ProductCard = () => {
+  return (
+    <div className='relative'>
+      <div className="bg-gray-300 rounded">
+        <img src="/headphone.png" alt="phone"
+          className='p-8 w-52'></img>
+        <div className="p-1 bg-gray-200 rounded-full absolute top-2 right-2">
+          <svg className="w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 21 19">
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11 4C5.5-1.5-1.5 5.5 4 11l7 7 7-7c5.458-5.458-1.542-12.458-7-7Z"/>
+          </svg>
+        </div>
+      </div>
+      <div className='flex justify-between'>
+        <p className='font-bold'>Wireless Earbuds</p>
+        <p className='font-bold'>$99.99</p>
+      </div>
+      <p className='text-xs'>Lorem ipsum dolor sit amet</p>
+      
+      {/* Stars */}
+      <div className="flex mb-4">
+        {
+          [1,2,3,4,5].map( (r)=>{
+            return (
+              <svg key={`${r}`} className="w-4 h-4 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+              </svg>
+            )
+          })
+        }
+      </div>
+
+      <Link className="rounded-full px-4 py-2 border text-black font-semibold hover:bg-gray-200" href="">
+        Add to Cart
+      </Link>
+      
+
+    </div>
+  )
+}
 export default function Home() {
   return (
     <main className="bg-white text-black px-24">
@@ -16,34 +55,40 @@ export default function Home() {
       </div>
 
       <div className='flex gap-4'>
-        <div className="rounded-3xl bg-slate-300 w-fit text-xs  font-bold py-2 px-4">
-          Headphone Type
-        </div>
+        <select className="rounded-3xl bg-gray-300 w-fit text-xs  font-bold py-2 px-2">
+          <option value="Headphones">Headphones </option>
+        </select>
 
-        <div className="rounded-3xl bg-slate-300 w-fit text-xs  font-bold py-2 px-4">
-          Price
-        </div>
+        <select className="rounded-3xl bg-gray-300 w-fit text-xs  font-bold py-2 px-2">
+          <option value="Headphones">Price</option>
+        </select>
+        
+        <select className="rounded-3xl bg-gray-300 w-fit text-xs  font-bold py-2 px-2">
+          <option value="Headphones">Review</option>
+        </select>
 
-        <div className="rounded-3xl bg-slate-300 w-fit text-xs  font-bold py-2 px-4">
-          Review
-        </div>
+        <select className="rounded-3xl bg-gray-300 w-fit text-xs  font-bold py-2 px-2">
+          <option value="Headphones">Colour</option>
+        </select>
 
-        <div className="rounded-3xl bg-slate-300 w-fit text-xs  font-bold py-2 px-4">
-          Colour
-        </div>
-
-        <div className="rounded-3xl bg-slate-300 w-fit text-xs  font-bold py-2 px-4">
-          Material
-        </div>
+        <select className="rounded-3xl bg-gray-300 w-fit text-xs  font-bold py-2 px-2">
+          <option value="Headphones">Material</option>
+        </select>
       </div>
 
       <h2 className='my-4 font-bold'>Headphones For You!</h2>
 
-      <div className='flex gap-4'>
-        <div className='w-44 h-56 rounded bg-slate-300'></div>
-        <div className='w-44 h-56 rounded bg-slate-300'></div>
-        <div className='w-44 h-56 rounded bg-slate-300'></div>
-        <div className='w-44 h-56 rounded bg-slate-300'></div>
+      {/* Product Cards */}
+      <div className='flex gap-4 flex-wrap'>
+
+        <ProductCard></ProductCard>
+        <ProductCard></ProductCard>
+        <ProductCard></ProductCard>
+        <ProductCard></ProductCard>
+        <ProductCard></ProductCard>
+        <ProductCard></ProductCard>
+        <ProductCard></ProductCard>
+        
       </div>
     </main>
   )
