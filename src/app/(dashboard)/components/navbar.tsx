69ba839d572a618b082d6480ef30d5e9c21f94b8
Chirag <chirag.dev18@gmail.com>
2023-07-19 18:11:46 +0530

    added skeleton loader on Images

--- src/app/(dashboard)/components/navbar.tsx
+++ src/app/(dashboard)/components/navbar.tsx
@@ -4,18 +4,16 @@ import Link from "next/link"
 
 export default function Navbar() {
     return (
-      <nav className="flex py-4 justify-between bg-white text-black px-24">
-        
+      <nav className="flex py-4 justify-between items-center bg-white text-black px-24">
           <Image src="/vercel.svg" alt="logo" width={100} height={240}/>
-        
         <ul className="flex mx-4">
             <li className="px-4 py-2"><Link href="/">Categories</Link></li>
             <li className="px-4 py-2"><Link href="/">Deals</Link></li>
             <li className="px-4 py-2"><Link href="/">{`What's New`}</Link></li>
             <li className="px-4 py-2"><Link href="/">Delivery</Link></li>
         </ul>
-        <div className="flex items-center gap-4 bg-gray-200 rounded-3xl px-4 py-1">
-          <input type="search" className=" w-64 bg-transparent focus:outline-none" placeholder="Search Product" />
+        <div className="flex items-center gap-4 bg-gray-200 rounded-3xl px-4 py-2 h-fit">
+          <input type="search" className=" w-64 bg-transparent focus:outline-none " placeholder="Search Product" />
           <div >
             <Image src="/search.svg" alt="btn" width={24} height={16}
               className="hover:cursor-pointer"
@@ -27,7 +25,7 @@ export default function Navbar() {
           </div>
         </div>
 
-        <div className="mt-2 flex">  
+        <div className="flex">  
             <Link href="/" className="mr-8 flex gap-2">
               <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 18">
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 8a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm-2 3h4a4 4 0 0 1 4 4v2H1v-2a4 4 0 0 1 4-4Z"/>
--- src/app/(dashboard)/product/page.tsx
+++ src/app/(dashboard)/product/page.tsx
@@ -4,8 +4,6 @@ import { useEffect, useState } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import { useSearchParams } from 'next/navigation'
-import ProductCard from '../components/productCard'
-
 
 export default function Product() {
   const [noOfItem, setnoOfItems] = useState(0)
@@ -33,7 +31,7 @@ export default function Product() {
             setProductData({...json})
     })
     console.log(1)
-  },[])
+  },[productID])
   return (
     <main className="bg-white text-black px-24 h-screen">
       {/* Navigation Route */}
@@ -44,7 +42,25 @@ export default function Product() {
       <div className="flex">
         <div className='w-1/2'>
             <div className="w-full h-[25rem] bg-gray-300 flex justify-center rounded relative">
-                <Image src={productData.image} alt="phone" className='p-8 mix-blend-multiply object-contain' fill />
+                {
+                  productData.id ?
+                    <Image 
+                      src={productData.image} 
+                      alt={productData.category}
+                      priority 
+                      fill
+                      className={`mix-blend-multiply object-contain ${productData.id ? 'p-8' : 'p-28'}`} 
+                    />
+                  :
+                    <div role="status" className="space-y-8 animate-pulse md:space-y-0 md:space-x-8 md:flex md:items-center">
+                        <div className="flex items-center justify-center w-full h-48 bg-gray-300 rounded sm:w-96 dark:bg-gray-700">
+                        <svg aria-hidden="true" className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                        </svg>
+                        </div>
+                    </div>
+                }
                 <div className="p-1 bg-gray-200 rounded-full absolute top-2 right-2">
                     <svg className="w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 21 19">
                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11 4C5.5-1.5-1.5 5.5 4 11l7 7 7-7c5.458-5.458-1.542-12.458-7-7Z"/>
@@ -53,17 +69,33 @@ export default function Product() {
             </div>
             {/* Product Cards */}
             <div className='flex justify-between mt-4'>
-                <ProductCard image={productData.image}></ProductCard>
-                <ProductCard image={productData.image}></ProductCard>
-                <ProductCard image={productData.image}></ProductCard>
-                <ProductCard image={productData.image}></ProductCard>
+              {    
+                [1,2,3,4].map((item) => {
+                    return (
+                      <div className="bg-gray-300 rounded relative w-28 h-28" key={item}>
+                        {
+                          productData.id ?
+                            <Image src={productData.image} alt="Product-Image" className='p-2 mix-blend-multiply object-contain' fill />
+                          :
+                          <div role="status" className="space-y-8 animate-pulse md:space-y-0 md:space-x-8 md:flex md:items-center">
+                            <div className="flex items-center justify-center w-full h-28 bg-gray-300 rounded sm:w-96 dark:bg-gray-700">
+                              <svg aria-hidden="true" className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                  <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                                  <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                              </svg>
+                            </div>
+                          </div>
+                        }
+                      </div>)
+                })
+              }
             </div>
         </div>
 
         {/* Content Side */}
         <div className='w-1/2 pl-12'>
             <p className='text-2xl font-bold'>{productData.title}</p>
-            <p className='text-sm'>{productData.description}</p>
+            <p className='text-xs mt-2'>{productData.description}</p>
             
             {/* Stars */}
             <div className="flex mb-4 mt-2 items-center">{
@@ -77,14 +109,13 @@ export default function Product() {
                 <p className='ml-1'>({productData.rating.count})</p>
             </div>
                 <hr/>
-                <p className="font-bold text-xl mt-4">
-                    {productData.price}$
+                <p className="font-bold text-xl my-4">
+                    Price : ${productData.price}
                 </p>
-                <p className='text-sm mb-4'>Lorem ipsum dolor sit amet consectetur </p>
                 
                 <hr/>
 
-                <div className='flex mb-4'>
+                <div className='flex my-4'>
                   <div className="flex items-center gap-4 bg-gray-200 rounded-full w-fit h-fit">
                     <button 
                       className="text-2xl px-4 py-1 "
@@ -104,7 +135,6 @@ export default function Product() {
                     </p>
                     <p>{`Don't miss it`}</p>
                   </div>
-                  
                 </div>
                 
                 {/* Action Buttons */}
